0ffaccf9bc2fee1e80ff0df70bbc8de1153afe53
manish9966 <manishsingh9966@gmail.com>
2022-10-17 16:16:51 +0530

    I am committing findRestaurantByName() method in RestaurantService.java class

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -6,8 +6,13 @@ public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
     public Restaurant findRestaurantByName(String restaurantName){
+        for (Restaurant restaurant : restaurants){
+            if (restaurant.getName().equals(restaurantName)){
+                return restaurant;
+            }
+        }
         return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+
     }
 
 
